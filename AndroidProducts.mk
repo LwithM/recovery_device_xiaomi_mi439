c667303cebcd1e3c86a2274c96563af2fabc26af
Lanmiemie <88493625+lanmiemie@users.noreply.github.com>
2023-01-15 11:20:33 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright 2023 The Android Open Source Project
 # Copyright (C) 2023 PitchBlack Recovery Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
